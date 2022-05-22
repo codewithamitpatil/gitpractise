1b9b542b7d3abecdd5194ea9c930e571a79cec6b
codewithamitpatil <pamit8831@gmail.com>
2022-05-22 13:05:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+const port = process.env.PROT || 4000;
+
+const app = express();
+
+app.use(cors('*'));
+
+// welcome route
+app.get('/', async (req, res) => {
+    res.send('hello fro docke practise');
+})
+
+// start server
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
+
+// https://github.com/codewithamitpatil/gitpractise.git
